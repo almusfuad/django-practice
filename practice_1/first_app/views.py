c4ab6f561f54e7c5059f846451984d743a2211f1
CS-D-42-16-104047 <almusfuad@students.diu.ac>
2023-11-29 03:30:22 +0600

    practiced geeksforgeeks

--- practice_1/first_app/views.py
+++ practice_1/first_app/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 
 
 def geeksfor(request):
-      return render(request, 'geeksfor.html')
+      context_dict = {'name': 'Rahim',
+                      'age': 20,
+                      'numbers': [1, 2, 3, 4, 5],
+                      'list': ['I', 'am', 'learning', 'django'],
+                      'quato': 'i\'m Rahim',
+                      'courses': [
+                            {'name': 'python', 'teacher': 'Rahim', 'length': 5, 'fees': 1000},
+                            {'name': 'django', 'teacher': 'Rahim', 'length': 4, 'fees': 2000},
+                      ],
+                      'sentence': 'Programming is fun',
+                      'date': datetime.datetime.now(),
+                      'empty': "",
+                      
+                  }
+      
+      
+      return render(request, 'geeksfor.html', context=context_dict)
 
 def earthly(request):
       return render(request, 'earthly.html')
