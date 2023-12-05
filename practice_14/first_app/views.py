80ece399f3785f10ab75ef7e9fb89e04c747f04c
almusfuad <almusfuad@gmail.com>
2023-12-06 02:06:58 +0600

    ready forms

--- practice_14/first_app/views.py
+++ practice_14/first_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from first_app.forms import UserForm
 
 # Create your views here.
 def home(request):
-      return render(request, 'home.html')
+      form = UserForm()
+      return render(request, 'home.html', {'form': form})
