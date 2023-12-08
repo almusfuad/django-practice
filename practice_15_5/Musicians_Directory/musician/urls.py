c72261dc0c5c2f44f418b858d6cb7bdb61abccc0
almusfuad <almusfuad@gmail.com>
2023-12-08 21:50:55 +0600

    data visualize and update complet

--- practice_15_5/Musicians_Directory/musician/urls.py
+++ practice_15_5/Musicians_Directory/musician/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-      path('add_musician/', views.add_musician, name = 'add_musician')
+      path('add_musician/', views.add_musician, name = 'add_musician'),
+      path('edit/<int:id>', views.edit_musician, name = 'edit_musician'),
 ]
