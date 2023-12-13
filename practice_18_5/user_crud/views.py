ebc8f21cad0fbfc765a773c214ff5b9895241f50
almusfuad <almusfuad@gmail.com>
2023-12-13 06:03:12 +0600

    user_crud account create successfull

--- practice_18_5/user_crud/views.py
+++ practice_18_5/user_crud/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from . import forms
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
       return render(request, 'home.html')
+
+
+def signup(request):
+      if request.method == 'POST':
+            signup_form = forms.SignUpForm(request.POST)
+            if signup_form.is_valid():
+                  signup_form.save()
+                  messages.success(request, 'Account created successfully.')
+                  messages.error(request, 'Error creating account.')         
+      else:
+            signup_form = forms.SignUpForm()
+      return render(request, 'signup.html', {'form': signup_form})
