5b2465f475828f368d929f3ae52d77a0a0213e98
almusfuad <almusfuad@gmail.com>
2023-12-16 00:34:41 +0600

    login successfull

--- practice_19_5/musicians_directory/user/views.py
+++ practice_19_5/musicians_directory/user/views.py
@@ -1,19 +1,44 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from . import forms
 from django.views import View
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 # Create your views here.
-class UserSignUpView(View):
-      template_name = 'signup.html'
-      form_class = UserCreationForm
+class UserRegisterView(View):
+      template_name = 'authentication.html'
+      form_class = forms.RegisterForm
       
       def get(self, request, *args, **kwargs):
-            sign_form = self.form_class()
-            return render(request, self.template_name, {'form': sign_form})
+            register_form = self.form_class()
+            return render(request, self.template_name, {'form': register_form, 'type': 'Register'})
       
       def post(self, request, *args, **kwargs):
-            sign_form = self.form_class(request.POST)
-            if sign_form.is_valid():
-                  sign_form.save()
-                  return redirect('signup')
-            return render(request, self.template_name, {'form': sign_form})
+            register_form = self.form_class(request.POST)
+            if register_form.is_valid():
+                  register_form.save()
+                  return redirect('login')
+            return render(request, self.template_name, {'form': register_form, 'type': 'Register'})
+      
+      
+class UserLoginView(LoginView):
+      template_name = 'authentication.html'
+      form_class = AuthenticationForm
+      
+      def get_success_url(self):
+            return reverse_lazy('login')
+      
+      def form_valid(self, form):
+            messages.success(self.request, 'Logged in successfully.')
+            return super().form_valid(form)
+      
+      def form_invalid(self, form):
+            messages.error(self.request, 'Logged in Failed.')
+            return super().form_valid(form)
+      
+      def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['type'] = 'Login'
+            return context
