f695e93423ce055f101091371475f0db8f0630a2
almusfuad <almusfuad@gmail.com>
2023-12-30 10:33:52 +0600

    task 1 complete

---
+++ practice_22_5/bank_management/transactions/constants.py
@@ -0,0 +1,15 @@
+DEPOSIT = 1
+WITHDRAWAL = 2
+LOAN = 3
+LOAN_PAID = 4
+TRANSFER_AMOUNT = 5
+
+
+
+TRANSACTION_TYPE = (
+      (DEPOSIT, 'Deposit'),
+      (WITHDRAWAL, 'Withdrawal'),
+      (LOAN, 'Loan'),
+      (LOAN_PAID, 'Loan Paid'),
+      (TRANSFER_AMOUNT, 'Transfer Amount'),
+)
